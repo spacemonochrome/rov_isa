bed3be7d4e1f9eab70e3c313db78587228e2d139
spacemonochrome <hfk4747@gmail.com>
2023-03-06 01:02:17 +0300

    .

--- cember/circle_detect.py
+++ cember/circle_detect.py
@@ -4,13 +4,18 @@ import numpy as np
 import time
 
 roi = None
+kaideli_diziler = []
+prev_frame_time = 0
+new_frame_time = 0
+kaydedilmis_cember_value = []
+onay_alan = 0
+onay_oran = 0
 
 def kamera_islem(goruntu):
     global roi
     roi = goruntu
-    color_g = np.array([20,50,50]), np.array([80,255,255])
-    main_islem(color_g)     
-    return roi
+    color_g = np.array([25,50,50]), np.array([80,255,255])
+    return main_islem(color_g)
 
 def main_islem(color_g):
     global roi
@@ -22,16 +27,42 @@ def main_islem(color_g):
     cam_orta_nokta_x, cam_orta_nokta_y = int(resim_genislik/2), int(resim_yukseklik/2)
     motor_hareket = None
     if len(contours) > 0:
-        odak = goruntu_tarama(resim_yukseklik, resim_genislik, contours, hiyerarsi)
+        odak = cember_tarama(resim_yukseklik, resim_genislik, contours, hiyerarsi)
         if odak is not None:
-            uzaklikx, uzakliky, motor_hareket = nesneye_kalibre(cam_orta_nokta_x, cam_orta_nokta_y, odak)
-    return roi
+            x, y = kalibrasyon(odak, cam_orta_nokta_x, cam_orta_nokta_y)
+            return roi, x, y
+    return roi, None, None
 
-def goruntu_tarama(resim_yukseklik, resim_genislik, contours, hiyerarsi):
-    odak = cember_tarama(resim_yukseklik, resim_genislik, contours, hiyerarsi)
-    return odak
+def kalibrasyon(odak, cam_orta_nokta_x, cam_orta_nokta_y):
+    global roi
+    global onay_alan
+    global onay_oran
+    resim_yukseklik,resim_genislik,_ = roi.shape
+    artanpay = resim_genislik-resim_yukseklik
+    a = int(resim_yukseklik/2 + resim_yukseklik*0.3 + artanpay / 2)
+    b = int(resim_yukseklik/2 + resim_yukseklik*0.3)
+    c = int(resim_yukseklik/2 - resim_yukseklik*0.3 + artanpay / 2)
+    d = int(resim_yukseklik/2 - resim_yukseklik*0.3)
+    (nesne_yukseklik, nesne_genislik, angle_rect, nesne_orta_nokta_x, nesne_orta_nokta_y, bnd_x, bnd_y, bnd_w, bnd_h, nesne_box, rect) = odak
+    cemberi_ciz(odak,(0,0,255), "")
+    xuzaklik = nesne_orta_nokta_x - cam_orta_nokta_x
+    yuzaklik = cam_orta_nokta_y - nesne_orta_nokta_y
+    cv2.putText(roi,"x: " + str(xuzaklik) + " y: " + str(yuzaklik), (cam_orta_nokta_x + 10, cam_orta_nokta_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+    
+    onay_alan = bnd_w*bnd_h*4
+    onay_oran = int((kisaolan(bnd_w,bnd_h)/uzun_olan(bnd_w,bnd_h))*100)
+    if onay_alan < a*a:
+        cv2.rectangle(roi,(a , b),(c, d),(0,0,0),3)
+    elif onay_alan > a*a and onay_oran > 80:
+        cv2.rectangle(roi,(a , b),(c, d),(0,0,255),3)
+        #duz ileriye gidecek kapali akil ile 85 85 80 80 85 85
+    
+    if()
+
+    return xuzaklik,yuzaklik
 
 def cember_tarama(resim_yukseklik, resim_genislik, contours, hiyerarsi):
+    global roi
     tum_dizi = []
     parent_diziler = []
     for hiyer_sayi in range(0,len(hiyerarsi[0])): #kaideli parentler bulunur.
@@ -47,7 +78,7 @@ def cember_tarama(resim_yukseklik, resim_genislik, contours, hiyerarsi):
     for parent_kontur in range(0,len(parent_diziler)): #kaideli parentlerin kaideli childlerini bulur        
         gruplama_dizi = []
         Parent_ilk_cocuk = hiyerarsi[0][parent_diziler[parent_kontur]][2]
-        while Parent_ilk_cocuk != -1:            
+        while Parent_ilk_cocuk != -1:
             nesne_yukseklik, nesne_genislik, angle_rect, nesne_orta_nokta_x, nesne_orta_nokta_y, bnd_x, bnd_y, bnd_w, bnd_h, nesne_box, rect = size_data(contours[Parent_ilk_cocuk])
             if nesne_yukseklik > resim_yukseklik * 0.01 and nesne_genislik > resim_genislik * 0.01: #burada contours size da iş görebilir.
                 gruplama_dizi.append(Parent_ilk_cocuk)
@@ -57,23 +88,63 @@ def cember_tarama(resim_yukseklik, resim_genislik, contours, hiyerarsi):
 
     for teker_ in tum_dizi:
         if len(teker_) > 1:
-            arayis = en_buyuk_child_konturu_bul(teker_,contours)
-            sonuc, kritik_degerler = sekil_arastirma(resim_yukseklik, resim_genislik, contours[arayis])
+            sonuc, kritik_degerler = sekil_arastirma(resim_yukseklik, resim_genislik, contours[en_buyuk_child_konturu_bul(teker_,contours)])
             if sonuc < 85:
                 sonuc, kritik_degerler = sekil_arastirma(resim_yukseklik, resim_genislik,
-                birlestirme_kodu(
-                buyukten_kucuge_2_li_sirala(teker_,contours,
-                en_buyuk_child_konturu_bul(teker_,contours)), contours))
-                if sonuc > 60:
+                birlestirme_kodu(buyukten_kucuge_2_li_sirala(teker_,contours,en_buyuk_child_konturu_bul(teker_,contours)), contours))
+                if sonuc > 55:
                     cemberi_ciz(kritik_degerler,(255,0,0),sonuc)
-            if sonuc > 85:
-                cemberi_ciz(kritik_degerler,(0,0,255),sonuc)
+                    kaydedilmis_cember_value.append(kritik_degerler)
+            elif sonuc > 85:
+                cemberi_ciz(kritik_degerler,(0,255,0),sonuc)
+                kaydedilmis_cember_value.append(kritik_degerler)
         elif len(teker_) == 1:
             sonuc, kritik_degerler = sekil_arastirma(resim_yukseklik, resim_genislik, contours[teker_[0]])
             if sonuc > 85:
-                cemberi_ciz(kritik_degerler,(0,0,255),sonuc)
+                cemberi_ciz(kritik_degerler,(0,255,0),sonuc)
+                kaydedilmis_cember_value.append(kritik_degerler)
 
-    return None
+    kabuledilmiscircle = []
+    grup_cember = []
+    gidecekveri = None
+    enbuyukcemberno = None
+    if len(kaydedilmis_cember_value) > 0: #cemberleri dikey olarak gruplar
+        for cember1 in range(0,len(kaydedilmis_cember_value)):
+            for cember2 in range(0,len(kaydedilmis_cember_value)):
+                (_, _, _, _, _, bnd_x1, bnd_y1, bnd_w1, bnd_h1, _, _) = kaydedilmis_cember_value[cember1]
+                (_, _, _, _, _, bnd_x2, bnd_y2, bnd_w2, bnd_h2, _, _) = kaydedilmis_cember_value[cember2]
+                if (cember1 != cember2) and dizide_deger_true_false(kabuledilmiscircle, cember1, cember2) and ((bnd_x1 < bnd_x2 < bnd_x1 + bnd_w1) or (bnd_x1 < bnd_x2 + bnd_w2 < bnd_x1 + bnd_w1)):
+                    grup_cember.append([cember1,cember2])
+                    kabuledilmiscircle.append(cember1)
+                    kabuledilmiscircle.append(cember2)
+                    
+        for cembera in range(0,len(kaydedilmis_cember_value)): #gruplanmayan cemberleri gruplar
+            if dizide_deger_true_falseso(kabuledilmiscircle,cembera):
+                grup_cember.append([cembera,cembera])
+                kabuledilmiscircle.append(cembera)
+
+        enbuyukcemberno = enbuyukcemberibul(kaydedilmis_cember_value) #en buyuk alanli cemberi bulur
+        bubm = dizide_deger_true_falsesonedo(grup_cember, enbuyukcemberno)
+        (_, _, _, _, _, bnd_x9, bnd_y9, bnd_w9, bnd_h9, _, _) = kaydedilmis_cember_value[bubm[0]]
+        (_, _, _, _, _, bnd_x8, bnd_y8, bnd_w8, bnd_h8, _, _) = kaydedilmis_cember_value[bubm[1]]
+        if bubm[0] == bubm[1]:
+            gidecekveri = kaydedilmis_cember_value[bubm[1]] #bubm1i baz al
+        else:
+            if bnd_y8 > bnd_y9:
+                gidecekveri = kaydedilmis_cember_value[bubm[1]]
+            elif bnd_y8 < bnd_y9:
+                gidecekveri = kaydedilmis_cember_value[bubm[0]]
+    return gidecekveri
+
+def enbuyukcemberibul(kaydedilmis_cember_value):
+    enbuyukalan = 0
+    secili_cember = None
+    for i in range(0,len(kaydedilmis_cember_value)):
+        (_, _, _, _, _, bnd_xa, bnd_ya, bnd_wa, bnd_ha, _, _) = kaydedilmis_cember_value[i]
+        if bnd_wa * bnd_ha > enbuyukalan:
+            enbuyukalan = bnd_wa * bnd_ha
+            secili_cember = i
+    return secili_cember
 
 def sekil_arastirma(resim_yukseklik, resim_genislik, gelen_kontur):
     ust_sinir = 400
@@ -129,28 +200,6 @@ def hesaplama(ctr,veriler):
                 dogruluk = False
     return int((sayac/len(ctr))*100)
 
-def nesneye_kalibre(cam_orta_nokta_x, cam_orta_nokta_y, odak):
-    global roi
-    nesne_orta_nokta_x, nesne_orta_nokta_y,bnd_w, bnd_h,nesne_yukseklik, nesne_genislik, sonuci, orani = odak
-    motor_hareket = None,None,None,None,None,None,None #hedef isabetlendi, motor1,motor2,motor3,motor4,motor5,motor6
-    uzaklikx = -(cam_orta_nokta_x - nesne_orta_nokta_x)
-    uzakliky = cam_orta_nokta_y - nesne_orta_nokta_y
-    ar, ra, _= roi.shape
-    h = None
-    cv2.putText(roi, "X:" + str(uzaklikx) + " Y:" + str(uzakliky), (cam_orta_nokta_x,cam_orta_nokta_y), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0,0,0), 1, cv2.LINE_AA)
-    if orani > 80:
-        if bnd_w < 200 and bnd_h < 200:
-            pt1 = (nesne_orta_nokta_x-100,nesne_orta_nokta_y-100)
-            pt2 = (nesne_orta_nokta_x+100,nesne_orta_nokta_y+100)
-        else:
-            pt1 = (int(nesne_orta_nokta_x-(15 + bnd_w*0.1)),int(nesne_orta_nokta_y-(15 + bnd_h*0.1)))
-            pt2 = (int(nesne_orta_nokta_x+(15 + bnd_w*0.1)),int(nesne_orta_nokta_y+(15 + bnd_h*0.1)))
-        cv2.rectangle(roi, pt1,pt2,(255,0,0), 2)
-        motor_hareket = "git",100,0,100,100,0,100
-    elif nesne_orta_nokta_x+nesne_genislik > cam_orta_nokta_x > nesne_orta_nokta_x-nesne_genislik and nesne_orta_nokta_y+nesne_yukseklik > cam_orta_nokta_y > nesne_orta_nokta_y-nesne_yukseklik:
-        pass
-    return uzaklikx, uzakliky, motor_hareket
-
 def size_data(ctr):
     rect = cv2.minAreaRect(ctr)
     nesne_yukseklik, nesne_genislik = int(rect[1][0]/2),int(rect[1][1]/2)
@@ -177,6 +226,12 @@ def uzun_olan(k,l):
         return k
     else:
         return l
+    
+def kisaolan(k,l):
+    if k > l:
+        return l
+    else:
+        return k
 
 def ebob(i,k):
     if i >= k:
@@ -234,4 +289,47 @@ def cemberi_ciz(kritik_degerler,renk,sonuc):
     global roi
     (nesne_yukseklik, nesne_genislik, angle_rect, nesne_orta_nokta_x, nesne_orta_nokta_y, bnd_x, bnd_y, bnd_w, bnd_h, nesne_box, rect) = kritik_degerler
     cv2.ellipse(roi, (nesne_orta_nokta_x,nesne_orta_nokta_y), (nesne_genislik,nesne_yukseklik), 270+angle_rect, 0, 360, renk, 2)
-    cv2.putText(roi, "%"+ str(sonuc) + "e - %"+ str(int(ebob(nesne_yukseklik,nesne_genislik)*100)) + "c", (nesne_orta_nokta_x,nesne_orta_nokta_y), cv2.FONT_HERSHEY_SIMPLEX,0.5, renk, 1, cv2.LINE_AA)
+    if sonuc != "":
+        cv2.putText(roi, "%"+ str(sonuc) + "e - %"+ str(int(ebob(nesne_yukseklik,nesne_genislik)*100)) + "c", (nesne_orta_nokta_x,nesne_orta_nokta_y), cv2.FONT_HERSHEY_SIMPLEX,0.5, renk, 1, cv2.LINE_AA)
+
+
+def dizide_deger_true_false(dizi, aranendeger1, aranendeger2):
+    if len(dizi) == 0:
+        return True
+    for a in dizi:
+        if a == aranendeger1 or a == aranendeger2:
+            return False
+    return True
+
+def dizide_deger_true_falsesonedo(dizi, aranendeger):
+    if len(dizi) == 0:
+        return True
+    for a in dizi:
+        if a[0] == aranendeger or a[1] == aranendeger:
+            return [a[0],a[1]]
+    return None
+
+def dizide_deger_true_falseso(dizi, aranendeger):
+    if len(dizi) == 0:
+        return True
+    for a in dizi:
+        if a == aranendeger:
+            return False
+    return True
+
+def baslangic():
+    global prev_frame_time, new_frame_time
+    roi = cv2.imread("a.png")
+    resim_yukseklik, resim_genislik, _ = roi.shape
+    roi, x, y = kamera_islem(roi)  
+    roi = kamera_xy_cizgi(roi)  
+    fps, prev_frame_time, new_frame_time = fps_func(prev_frame_time,new_frame_time)
+
+    cv2.putText(roi, fps + " FPS", (15,15), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0,0,255), 1, cv2.LINE_AA)
+
+    cv2.imshow('tespit',roi)
+    cv2.imwrite('cikti.png',roi)
+    cv2.waitKey()
+
+if __name__ == '__main__':
+    baslangic()
